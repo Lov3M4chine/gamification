81520eb9157ab9d6d9f1c1857b8538c6be15b65b
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-06-21 08:25:12 -0600

    Create experience and credit structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './styles.css';
+
+let experienceNeeded = [];
+let experienceGrantedPerMinute = [];
+let creditsGrantedPerMinute = [];
+
+function calculateExperienceForEachLevel() {
+    let experienceNeededCounter = 0;
+    for (let i = 1; i < 11; i++) {
+        let calculateExperienceNeeded = (i * 1000) * 1.5;
+        experienceGrantedPerMinute.push((calculateExperienceNeeded / 100) / 60);
+        experienceNeededCounter += calculateExperienceNeeded;
+        experienceNeeded.push(experienceNeededCounter);
+
+    }
+    console.log(experienceNeeded);
+    console.log(experienceGrantedPerMinute);
+}
+
+function calculateCreditsForEachLevel() {
+    experienceGrantedPerMinute.forEach((experienceGrantedPerMinue) => {
+        creditsGrantedPerMinute.push(experienceGrantedPerMinue * 4);
+    });
+    console.log(creditsGrantedPerMinute);
+}
+
+function createNewPlayer() {
+    class Player {
+        constructor(name) {
+            this.name = name;
+            this.experience = 0;
+            this.credits = 100;
+            this.equipment = {};
+            this.inventory = {};
+            this.skills = {};
+        }
+    }
+}
