38a8664f82be1a25dde961736b908860b4f2998f
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-06-21 08:42:51 -0600

    Add custom Star Wars theme to daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ module.exports = {
     variants: {
       extend: {},
     },
+    daisyui: {
+      themes: [
+        {
+          starwars: {
+          "primary": "#4493c4",                    
+          "secondary": "#9f1239",                     
+          "accent": "#fde047",                      
+          "neutral": "#191b24",                      
+          "base-100": "#3d4651",                      
+          "info": "#166534",                      
+          "success": "#1ab3a6",                      
+          "warning": "#b1820b",                      
+          "error": "#9f1239",
+          },
+        },
+      ],
+    },
     plugins: [require("daisyui")],
   };
   
